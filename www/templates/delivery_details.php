3b667e583f4cdede22d2a6ac52027cf0f1662745
Maciek Kawecki <maciej@kaw.net.pl>
2017-03-16 10:48:20 +0100

    fix tpl

--- www/templates/delivery_details.php
+++ www/templates/delivery_details.php
@@ -3,7 +3,6 @@
 
 <div data-role="header" data-position="fixed">
 <a href="index.php?act=dl" data-icon="back" data-role="button" class="ui-btn-left show-page-loading-msg">Powrót</a>
-<h1 style="position:absolute;left:40%">Szczegóły</h1>
 <div id="userInfo" style="float:right" data-role="content"></div>
 </div>
 
--- www/templates/map_route.php
+++ www/templates/map_route.php
@@ -4,7 +4,6 @@
 <div data-role="header" id="mpHeader" data-position="fixed" style="z-index:9999">
 	<a href="index.php?act=dd&id=<?php echo $id; ?>" data-icon="back"
 	data-role="button" class="ui-btn-left show-page-loading-msg">Powrót</a>
-	<h1 style="position:absolute;left:40%">Szczegóły</h1>
 	<div id="userInfo" style="float:right" data-role="content"></div>
 </div>
 
